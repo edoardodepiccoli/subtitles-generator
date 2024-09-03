00187f17afa1db3ab8521c41a2fa597ead10f6e0
Edoardo De Piccoli <103844232+edoardodepiccoli@users.noreply.github.com>
2024-09-03 13:08:47 +0200

    connect and test openai api

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "dotenv", "~> 3.1"
+
+gem "httparty", "~> 0.22.0"
+
+gem "json", "~> 2.7"
--- main.rb
+++ main.rb
@@ -1,4 +1,28 @@
 require "dotenv"
+require "httparty"
+require "json"
 Dotenv.load
 
-API_KEY = ENV["API_KEY"] # this works
+# api_key = ENV["API_KEY"] # this works yay
+# audio_file_path = 'medias/audio.mp3'
+
+# response = HTTParty.post(
+#   "https://api.openai.com/v1/audio/transcriptions",
+#   headers: {
+#     "Authorization" => "Bearer #{api_key}"
+#   },
+#   body: {
+#     file: File.new(audio_file_path),
+#     model: "whisper-1",
+#     response_format: "verbose_json",
+#     "timestamp_granularities[]": "word"
+#   }
+# )
+
+# if response.code != 200
+#   puts "Error: #{response.body}"
+#   exit
+# end
+
+# json_response = JSON.parse(response.body)
+# segments = json_response["segments"]
